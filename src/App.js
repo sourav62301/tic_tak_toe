c69b1c47d18bb91735f00d5e3b52b61204265a44
Sourav <sourav862888@gmail.com>
2022-08-14 17:24:59 +0530

    Added board state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import Board from './components/Board';
+
+import './styles/root.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to
-          relooooooooooooooooooooooooooooooad.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <Board />
     </div>
   );
 }
