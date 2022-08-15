0a6c95fa8f057e5299f7bdbb38d179e0edae2c67
Sourav <sourav862888@gmail.com>
2022-08-15 16:37:23 +0530

    Added History

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
+import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
 
 function App() {
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+
+  // console.log('history', history);
+
+  // const [isXNext, setIsXNext] = useState(false);
+  // console.log('Board Re-render');
+
+  const winner = calculateWinner(current.board);
+  // console.log(winner);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next Player is ${current.isXNext ? 'X' : '0'}`;
+
+  const handleSquareClick = position => {
+    if (current.board[position] || winner) {
+      return;
+    }
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : '0';
+        }
+        return square;
+      });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+
+    // setIsXNext(prev => !prev);
+    setCurrentMove(prev => prev + 1);
+  };
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+      <h2>{message}</h2>
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Square = ({ value, onClick }) => {
+  // console.log('Square Re-render');
   return (
     <button type="button" className="square" onClick={onClick}>
       {value}
